5e2ef15837819c7bc473442dd6f010e79808e783
brantanpnc <brantan@pnc.com>
2019-07-21 21:17:43 +0530

    First commit

---
+++ com/src/main/practise/PythonBasics.py
@@ -0,0 +1,21 @@
+"""here is the demo for the python list"""
+#i can comment like this too
+'''and like this too'''
+
+list = ['king','queen','rook','bishop','horse','pawn']
+list.append('chess')
+list.insert(0,'game')
+print(list)
+
+tuples = ('red','black','white','carrom')
+print(tuples)
+
+if True:
+    print("if statement")
+else:
+    print("false");
+
+print("King")
+
+for i in range (0,100):
+    print(i);
